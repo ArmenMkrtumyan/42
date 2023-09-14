c02f9bf75e364c7a3c373ddd1f0d89e89d1b1a1d
Armen Mkrtumyan <amkrtumy@c1r2s6.42yerevan.am>
2023-09-14 12:45:30 +0400

    Adding so long

--- so_long/mlx.c
+++ so_long/mlx.c
@@ -8,16 +8,78 @@
 // #define WIDTH			400
 // #define HEIGHT			400
 
-int count_checker(char symbol)
+void free_matrix(char **matrix, int row)
 {
-	
+	int	i;
+
+	i = 0;
+	while(i < row) {
+    	free(matrix[i++]);
+	}
+	free(matrix);
+}
+
+int count_checker(char **matrix, int row, int column)
+{
+	int i = 0;
+	int k = 0;
+	char letters[4];
+
+	letters[0] = 0; //C
+	letters[1] = 0; //E
+	letters[2] = 0; //P
+	letters[3] = '\0';
+	while(i < row)
+	{
+		while(k < column)
+		{
+			if((matrix[i][k] != '1') && (k==0 || k==column-1 || i==0 || i == row-1))
+			{
+				free_matrix(matrix, row);
+				return (0);
+			}
+
+			if(matrix[i][k] == 'C')
+				letters[0] ++;
+			else if(matrix[i][k] == 'E')
+				letters[1] ++;
+			else if(matrix[i][k] == 'P')
+				letters[2] ++;
+			else
+			{
+				if(matrix[i][k] != '1' && matrix[i][k] != '0' )
+				{
+					free_matrix(matrix, row);
+					return(0);
+				}
+			}
+			k++;
+		}
+		i++;
+		k = 0;
+	}
+	if (letters[0] == 0 || letters[1] != 1 || letters[2] != 1)
+	{
+		free_matrix(matrix, row);
+		return(0);
+	}
+	free_matrix(matrix, row);
+	return (1);
 }
 
 int check_insides_map(int fd, int row, int column)
 {
 	int changing_rows;
 	int changing_columns;
-	char matrix[row+1][column+1];
+	char **matrix;
+
+	matrix = (char **)malloc((row+1) * sizeof(char*));
+	int k = 0;
+	while(k < row)
+	{
+		matrix[k] = (char *)malloc((column+1) * sizeof(char));
+		k++;
+	}
 	int sz;
 	char symbol[2];
 
@@ -28,34 +90,23 @@ int check_insides_map(int fd, int row, int column)
 	while(sz != 0)
 	{
 		sz = read(fd, symbol, 1);
-		if(symbol[0] == '\n' || sz == 0)
-		{
-			changing_rows+=1;
-			changing_columns = 0;
-		}
+
 		symbol[sz] = '\0';
 		if(sz == 0)
 			matrix[changing_rows][changing_columns] = '\0';
 		else
 			matrix[changing_rows][changing_columns] = symbol[0];
-		printf("%c", matrix[changing_rows][changing_columns]);
+		printf("%c", symbol[0]);
 		changing_columns++;
-	}
-	changing_rows = 0;
-	changing_columns = 0;
-	printf("\n\n\n\n\n\n");
-	while(changing_rows < row)
-	{
-		while(changing_columns <= column)
+		if(symbol[0] == '\n' || sz == 0)
 		{
-			count_checker(matrix[changing_rows][changing_columns]);
-			write(1, &matrix[changing_rows][changing_columns], 1);
-			changing_columns++;
+			changing_rows+=1;
+			changing_columns = 0;
 		}
-		changing_columns = 0;
-		changing_rows ++;
 	}
-	return (1);
+	if(count_checker(matrix, row, column))
+		return (1);
+	return (0);
 }
 
 int check_dimensions_map(int fd, int *row, int *column)
@@ -110,10 +161,13 @@ int	main()
 		printf("\nCORRECT MAP DIMENSIONS(%d,%d), CHECKING THE INSIDE...\n", row, column);
 
 		if(check_insides_map(fd2, row, column))
-			printf("\nCORRECT MAP INSIDES, LOADING THE GAME...");
-
+			printf("\nCORRECT MAP INSIDES (%d, %d), LOADING THE GAME...", row, column);
+		else
+				printf("\nWRONG MAP\n");
 	}
 	else
 		printf("\nWRONG MAP\n");
 
+	// while(1)
+	// 	;
 }
