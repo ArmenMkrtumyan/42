3de5885a3ae24597a7a280d55563baf58428ded6
Armen Mkrtumyan <amkrtumy@c2r11s3.42yerevan.am>
2023-12-27 14:47:51 +0400

    WORKS

--- philosophers/philosophers.c
+++ philosophers/philosophers.c
@@ -12,131 +12,152 @@
 
 #include "philosophers.h"
 
-int get_time()
+int get_time(struct timeval start_time)
 {
 	struct timeval te;
 	int milliseconds;
 	gettimeofday(&te, NULL);
-	milliseconds = te.tv_sec * 1000LL + te.tv_usec / 1000 - 1703610000000;
+	milliseconds = te.tv_sec * 1000LL + te.tv_usec / 1000
+		- (start_time.tv_sec * 1000LL + start_time.tv_usec / 1000);
 	return milliseconds;
 }
 
-void *initalizer(void *pack)
+void *initalizer(void *packed)
 {
-	t_fork_plus_philosopher* fork_philo;
-	fork_philo = (t_fork_plus_philosopher *)pack;
-	int current;
-	int time_passed;
+	t_philos	*pack;
+	int			current;
+	int			time_passed;
 
-	printf("Index of philosopher: %d\n", fork_philo->philosopher.index);
-	if (fork_philo->philosopher.index % 2 == 0)
-		usleep(10000);
+	pack = (t_philos *)packed;
+	if (pack->index % 2 == 0)
+		usleep(1500);
 	while (1)
 	{
-		time_passed = get_time();
-		pthread_mutex_lock(&(fork_philo->forks[fork_philo->philosopher.index - 1 % fork_philo->num_of_philo]));
-		printf("%d N%d has taken a fork\n", time_passed, fork_philo->philosopher.index);
-		time_passed = get_time();
-		pthread_mutex_lock(&(fork_philo->forks[fork_philo->philosopher.index + 1 % fork_philo->num_of_philo]));
-		printf("%d N%d has taken a fork\n", time_passed, fork_philo->philosopher.index);
-		printf("%d N%d is eating\n", time_passed, fork_philo->philosopher.index);
-		current = get_time();
-		while (time_passed - current < fork_philo->philosopher.time_to_eat)
-			time_passed = get_time();
-		fork_philo->philosopher.last_time_eaten = time_passed;
-		pthread_mutex_unlock(&(fork_philo->forks[fork_philo->philosopher.index - 1 % fork_philo->num_of_philo]));
-		pthread_mutex_unlock(&(fork_philo->forks[fork_philo->philosopher.index + 1 % fork_philo->num_of_philo]));
-		current = get_time();
-		printf("%d N%d is sleeping\n", current, fork_philo->philosopher.index);
-		while (time_passed - current < fork_philo->philosopher.time_to_sleep)
-			time_passed = get_time();
-		printf("%d N%d is thinkig\n", time_passed, fork_philo->philosopher.index);
+		int a = 0;
+		time_passed = get_time(pack->start_time);
+		if ((a = pthread_mutex_lock(&(pack->forks[pack->index - 1 % pack->num_of_philo])) != 0))
+			strerror(a);
+		// printf("The address %p is locked\n", &(pack->forks[pack->index - 1 % pack->num_of_philo]));
+		printf("%d ms N%d has taken a fork\n", time_passed, pack->index);
+		time_passed = get_time(pack->start_time);
+		pthread_mutex_lock(&(pack->forks[pack->index + 1 % pack->num_of_philo]));
+		// printf("The address %p is locked\n", &(pack->forks[pack->index + 1 % pack->num_of_philo]));
+		printf("%d ms N%d has taken a fork\n", time_passed, pack->index);
+		printf("%d ms N%d is eating\n", time_passed, pack->index);
+		current = get_time(pack->start_time);
+		while (time_passed - current < pack->philosopher.time_to_eat)
+			time_passed = get_time(pack->start_time);
+		pthread_mutex_unlock(&(pack->forks[pack->index - 1 % pack->num_of_philo]));
+		pthread_mutex_unlock(&(pack->forks[pack->index + 1 % pack->num_of_philo]));
+		pthread_mutex_lock(&(pack->last_time_mutex));
+		pack->last_time_eaten = time_passed;
+		pthread_mutex_unlock(&(pack->last_time_mutex));
+		// printf("The address %p is unlocked\n", &(pack->forks[pack->index - 1 % pack->num_of_philo]));
+		// printf("The address %p is unlocked\n", &(pack->forks[pack->index + 1 % pack->num_of_philo]));
+		current = get_time(pack->start_time);
+		printf("%d ms N%d is sleeping\n", current, pack->index);
+		while (time_passed - current < pack->philosopher.time_to_sleep)
+			time_passed = get_time(pack->start_time);
+		printf("%d ms N%d is thinking\n", time_passed, pack->index);
 	}
-	pthread_exit(NULL);
+
+	return (NULL);
 }
 
-t_philosopher get_values_from_argv(char *argv, int index)
+t_philosopher get_values_from_argv(char **argv)
 {
-	t_philosopher philo;
+	t_philosopher	philo;
 
-	philo.index = index;
-	philo.time_to_die = ft_atoi(&argv[0]);
-	philo.time_to_eat = ft_atoi(&argv[1]);
-	philo.time_to_sleep = ft_atoi(&argv[2]);
-	philo.eat_count = ft_atoi(&argv[3]);
-
+	// philo.index = index;
+	philo.time_to_die = ft_atoi(argv[2]);
+	philo.time_to_eat = ft_atoi(argv[3]);
+	philo.time_to_sleep = ft_atoi(argv[4]);
+	philo.eat_count = ft_atoi(argv[5]);
+	// printf("Creating a philo with index %d\n time_to_die: %d\n time_to_eat: %d\n time_to_sleep: %d\n eat_count: %d\n", philo.index, philo.time_to_die, philo.time_to_eat, philo.time_to_sleep, philo.eat_count);
 	return (philo);
+}
+
+int	on_exit(char *message)
+{
+	printf("%s", message);
+	return (0);
 }
 
 int main(int argc, char *argv[])
 {
+	int				num_of_philosophers;
+	int				i;
+	t_philosopher	philosopher;
+	pthread_mutex_t	*forks;
+	int				start_time;
+	t_philos		*array_of_philos;
 
-	int num_of_philosophers;
-	pthread_t *array_of_philos;
-	int i;
+	struct timeval	te;
+	struct timeval current;
+
 	// struct timeval te;
-	// gettimeofday(&te, NULL); // get current time
-	// int current;
-	t_philosopher philosopher;
-	pthread_mutex_t *forks;
-	t_fork_plus_philosopher combo;
+	gettimeofday(&te, NULL);// get current time
 
+	start_time = get_time(te);
 	if (argc != 6 && argc != 5)
-	{
-		printf("Invalid number of arguments. Expected 4 or 5 arguments.\n");
-		return 0;
-	}
+		return (on_exit("Invalid number of arguments. Expected 4 or 5 arguments.\n"));
 	if (argc == 5)
 		philosopher.eat_count = -1;
 	i = 1;
 	while (i < argc)
 	{
 		if (check_parsing(argv[i]) == -1)
-		{
-			printf("Invalid argument.\n");
-			return 0;
-		}
+			on_exit("Invalid argument.\n");
 		i++;
 	}
-
 	num_of_philosophers = ft_atoi(argv[1]);
-	array_of_philos = malloc(sizeof(pthread_t) * num_of_philosophers);
+	array_of_philos = malloc(sizeof(t_philos) * num_of_philosophers);
 	forks = malloc(sizeof(pthread_mutex_t) * num_of_philosophers);
+	i = -1;
+	while (++i < num_of_philosophers)
+	{
+		array_of_philos[i].last_time_eaten = 0;
+		pthread_mutex_init(&forks[i], NULL);
+		pthread_mutex_init(&(array_of_philos[i].last_time_mutex), NULL);
+	}
 	i = 0;
+	// printf("%d\n", array_of_philos[0].last_time_eaten);
 	while (i < num_of_philosophers)
 	{
-		philosopher = get_values_from_argv(*argv, i);
-		combo.philosopher = philosopher;
-		combo.forks = forks;
-		combo.num_of_philo = num_of_philosophers;
-		if (pthread_create(&array_of_philos[i], NULL, &initalizer, &combo) != 0)
+		philosopher = get_values_from_argv(argv);
+		array_of_philos[i].philosopher = philosopher;
+		array_of_philos[i].forks = forks;
+		array_of_philos[i].num_of_philo = num_of_philosophers;
+		array_of_philos[i].start_time = te;
+		array_of_philos[i].index = i;
+		if (pthread_create(&array_of_philos[i].thread_id, NULL, &initalizer, &array_of_philos[i]) != 0)
 			perror("Failed to created thread");
 		i++;
 	}
-	// int num = 0;
-
-	// gettimeofday(&te, NULL);
-	// current = te.tv_sec * 1000LL + te.tv_usec / 1000;
-	// while (1)
-	// {
-	// 	if (array_of_philos[num]->philosopher.last_time_eaten - current > array_of_philos[num]->philosopher.time_to_die)
-	// 	{
-	// 		printf("philosopher N%d died\n", array_of_philos[num]->philosopher.index);
-	// 		break;
-	// 	}
-	// 	gettimeofday(&te, NULL);
-	// 	current = te.tv_sec * 1000LL + te.tv_usec / 1000;
-	// 	num++;
-	// 	if (num == num_of_philosophers)
-	// 		num = 0;
-	// }
+	int num = 0;
+	while (1)
+	{
+		// gettimeofday(&current, NULL);
+		// current = get_time(te.tv_sec * 1000LL + te.tv_usec / 1000);
+		pthread_mutex_lock(&(array_of_philos[num].last_time_mutex));
+		// printf("The last time eaten by %d\n", get_time(te) - array_of_philos[num].last_time_eaten);
+		if (get_time(te) - array_of_philos[num].last_time_eaten > array_of_philos[num].philosopher.time_to_die)
+		{
+			printf("philosopher N%d died\n", array_of_philos[num].index);
+			break;
+		}
+		pthread_mutex_unlock(&(array_of_philos[num].last_time_mutex));
+		num++;
+		if (num == num_of_philosophers)
+			num = 0;
+	}
 	return 0;
+
 	// for (i = 0; i < 10; i++)
 	// {
-	// 	if (pthread_join(th[i], NULL) != 0)
+	// 	if (pthread_join(array_of_philos[i], NULL) != 0)
 	// 	{
 	// 		perror("Failed to join thread");
 	// 	}
 	// }
-	// return 0;
 }
--- philosophers/philosophers.h
+++ philosophers/philosophers.h
@@ -18,48 +18,62 @@
 // # include "libft.h"
 # include "ft_printf.h"
 # include <stdlib.h>
+# include <string.h>
 # include <stdio.h>
 # include <pthread.h>
 # include <unistd.h>
 # include <time.h>
 # include <sys/time.h>
 
-typedef struct s_philosopher
+typedef struct			s_philosopher
 {
-	int		index;
-	int		time_to_die;
-	int		time_to_eat;
-	int		time_to_sleep;
-	int		eat_count;
-	int		last_time_eaten;
-}			t_philosopher;
+	// int					index;
+	int					time_to_die;
+	int					time_to_eat;
+	int					time_to_sleep;
+	int					eat_count;
+	int					last_time_eaten;
+}						t_philosopher;
 
-typedef struct s_fork_plus_philosopher
+typedef struct			s_packed
 {
 	t_philosopher		philosopher;
 	pthread_mutex_t*	forks;
 	int					num_of_philo;
-}						t_fork_plus_philosopher;
+}						t_pack;
+
+typedef struct			s_array_of_philosophers
+{
+	int					num_of_philo;
+	int					index;
+	int					last_time_eaten;
+	pthread_t			thread_id;
+	t_philosopher		philosopher;
+	struct timeval		start_time;
+	pthread_mutex_t		last_time_mutex;
+	pthread_mutex_t*	forks;
+}						t_philos;
+
 
 // philosophers
 
-int				get_time();
-void			*initalizer(void *pack);
-t_philosopher	get_values_from_argv(char *argv, int index);
-t_philosopher	get_values_from_argv(char *argv, int index);
+int						get_time();
+void					*initalizer(void *pack);
+t_philosopher			get_values_from_argv(char **argv);
+int						on_exit(char *message);
 
 
 // atoi
 
-int		check_emptyness(int i, const char *str);
-void	check_sign(char str_i, int *sign, int *i);
-int		ft_atoi(char *str);
+int						check_emptyness(int i, const char *str);
+void					check_sign(char str_i, int *sign, int *i);
+int						ft_atoi(char *str);
 
 // checkers
 
-int		check_max(char *string, char *critical_value);
-int		check_if_number(char *element);
-int		check_parsing(char *element);
+int						check_max(char *string, char *critical_value);
+int						check_if_number(char *element);
+int						check_parsing(char *element);
 
 
 # endif
